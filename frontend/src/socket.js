52008b1268d791395ba21ae37b3b3a8bb50b2787
souravghoshofficial <souravghosh.tech@gmail.com>
2025-07-31 09:41:10 +0530

    modify the options in socket io client

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,10 +2,13 @@ import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
     const options = {
-        'force new connection': true,
-        reconnectionAttempt: 'Infinity',
-        timeout: 10000,
+        forceNew: true,
+        reconnection: true,
+        reconnectionAttempts: Infinity,
+        reconnectionDelay: 3000,
+        timeout: 30000,
         transports: ['websocket'],
+
     };
     return io(import.meta.env.VITE_BACKEND_URL, options);
 };
